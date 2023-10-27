1e0aa8643ef8ede98baee55ff8d1cc5cdfd3fc5b
Cristiano <cristianofull@gmail.com>
2023-10-27 18:27:10 -0300

    Entrega trilha net poo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia:");
+
+Smartphone Nokia = new Nokia ("9876-54321", "G21", "00000000", 128);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("WhatsApp");
+Console.WriteLine();
+
+Console.WriteLine("SmartPhone Iphone:");
+
+Smartphone Iphone = new Iphone ("93256-2154", "XR", "11111111", 128);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Telegram");
